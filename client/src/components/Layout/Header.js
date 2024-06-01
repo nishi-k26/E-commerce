3cd7bca0a82ffd7820c2ceb9636a217d7e20e1df
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-02 00:50:02 +0530

    Context Auth

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { NavLink,Link } from 'react-router-dom'
 import { LuShoppingCart } from 'react-icons/lu';
+import { useAuth } from '../../context/auth';
+import toast from 'react-hot-toast';
 
 const Header = () => {
+  const [auth, setAuth] = useAuth()
+  const handleLogout = () => {
+    setAuth({
+      ...auth, 
+      user:null, 
+      token:''
+    })
+    localStorage.removeItem("auth");
+    toast.success('Logout Success!');
+  }
   return (
     <>
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -21,12 +33,20 @@ const Header = () => {
         <li className="nav-item">
           <NavLink to='/category' className="nav-link">Category</NavLink>
         </li>
-        <li className="nav-item">
+        {
+          !auth.user? (<>
+          <li className="nav-item">
           <NavLink to='/register' className="nav-link" href="#">Register</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/login' className="nav-link" href="#">Login</NavLink>
-        </li>
+          </li>
+          <li className="nav-item">
+            <NavLink to='/login' className="nav-link" href="#">Login</NavLink>
+          </li>
+          </>) : (<>
+            <li className="nav-item">
+            <NavLink onClick={handleLogout} to='/login' className="nav-link" href="#">Logout</NavLink>
+          </li>
+          </>)
+        }
         <li className="nav-item">
           <NavLink to='/cart' className="nav-link" href="#">Cart [0]</NavLink>
         </li>
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,10 +4,12 @@ import toast from 'react-hot-toast';
 import axios from 'axios';
 import "../../styles/AuthStyles.css"
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/auth';
 
 const Login = () => {
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [auth, setAuth] = useAuth()
     const navigate = useNavigate();
     //form function
     const handleSubmit = async (e) => {
@@ -16,6 +18,12 @@ const Login = () => {
             const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`,{email,password});
             if(res && res.data.success){
                 toast.success(res.data.message);
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token,
+                })
+                localStorage.setItem('auth',JSON.stringify(res.data));
                 navigate('/')
             }
             else{
