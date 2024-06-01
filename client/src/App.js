f7ce826e692eff5106cd5cef58990d4d2108eb02
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-01 18:36:09 +0530

    Edits to remove errrors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import './App.css';
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
     <>
-    <Routes>
-    <Route path="/" element={<HomePage />}/>
-    </Routes>
+    <Layout>
+      <h1>E-commerce App</h1>
+    </Layout>
     </>
   );
 }
