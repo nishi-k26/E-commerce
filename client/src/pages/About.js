850ffc95d13a8d7b811abf4063b98c1ec0350fc7
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-01 20:55:32 +0530

    Edited Front-end

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,24 @@ import Layout from './../components/Layout/Layout';
 const About = () => {
   return (
     <Layout>
-        <h1>About</h1>
+      <div className="row contactus">
+        <div className="col-md-6">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{width: "100%"}}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text white text-centre">CONTACT US</h1>
+          <p className="text-justify mt-2">
+            About Us
+          </p>
+          <p className="mt-3">
+            This is About us page.
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
