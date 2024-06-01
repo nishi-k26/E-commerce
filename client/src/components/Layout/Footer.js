ad57ff11c0da5c85c0c98a564e9a4ba07a803e10
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-01 15:38:33 +0530

    Initial Commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+        <h5 className="text-center">All Rights Reserved &copy;</h5>
+    </div>
+  )
+}
+
+export default Footer
