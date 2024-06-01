5b28bc3026428206793b0a67e485ed269cfb50b1
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-01 21:13:54 +0530

    Edited Pages

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import {Helmet} from "react-helmet";
 
-const Layout = ({children}) => {
+const Layout = ({children, title, description, keywords, author}) => {
   return (
     <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+          <title>{title}</title>
+        </Helmet>
         <Header/>
         <main style={{minHeight: "70vh"}}>
         {children}
@@ -12,6 +20,13 @@ const Layout = ({children}) => {
         <Footer/>
     </div>
   )
+}
+
+Layout.defaultProps = {
+  title: 'E-Commerce App',
+  description: 'Mern Stack Project',
+  keywords: 'MongoDB, Express.js, React, Node.js',
+  author: 'Nishita',
 }
 
 export default Layout
