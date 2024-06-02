5d6b10ed5519d8b6a60b1c407c740ee5771248e4
Nishita Kumar <1ms21cs085@msrit.edu>
2024-06-03 00:16:06 +0530

    CRUD for category

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
+import { CategoryController, CreateCategoryController, DeleteCategoryController, SingleCategoryController, UpdateCategoryController } from '../controllers/categoryController.js';
+
+const router = express.Router()
+
+//create category
+router.post('/create-category',requireSignIn, isAdmin, CreateCategoryController)
+
+//update category
+router.put('/update-category/:id',requireSignIn, isAdmin, UpdateCategoryController)
+
+//getAll category
+router.get('/get-category', CategoryController)
+
+//single category
+router.get('/single-category/:slug', SingleCategoryController)
+
+//delete category
+router.delete('/delete-category/:id', requireSignIn, isAdmin, DeleteCategoryController)
+
+export default router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from'dotenv';
 import morgan from 'morgan';
 import connectDB from "./config/db.js";
+import categoryRoutes from './routes/categoryRoutes.js'
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 
@@ -22,6 +23,7 @@ app.use(morgan("dev"));
 
 //routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
 
 //rest api
 app.get('/', (req,res) => {
